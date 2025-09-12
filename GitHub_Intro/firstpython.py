1f6a455ac40d90168b4eaa3f05207fecea8edb64
FrankieVara <123497218+FrankieVara@users.noreply.github.com>
2025-09-11 21:55:33 -0700

    Create firstpython.py
    I am reviewing python and git to refresh my skills in coding!

---
+++ GitHub_Intro/firstpython.py
@@ -0,0 +1,6 @@
+#Comment is created with #
+
+#print is used to display text in the console
+
+print("Hello World")
+print("This is my first python file, that I have written since graduating in May of 2024")
